8a31ba2b2d50d132eb20eafdb401814993dd53ab
Styoks <liina.hp96@gmail.com>
2024-11-05 17:01:56 +0000

    Added my places

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,12 @@ import {
   Redirect,
   Switch,
 } from "react-router-dom";
+
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
+
 import "./App.css";
 
 const App = () => {
@@ -18,6 +21,9 @@ const App = () => {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
--- frontend/src/user/components/UsersList.js
+++ frontend/src/user/components/UsersList.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import UsersItem from "./UserItem";
-import Card from "../../shared/components/UIElements/Card";
 import "./UsersList.css";
 
 const UsersList = (props) => {
